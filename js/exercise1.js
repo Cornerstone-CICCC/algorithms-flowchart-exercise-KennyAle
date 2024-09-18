4f054984e944b7e38f8352a9157fc1495c93b81b
KennyAle <kennyviver@gmail.com>
2024-09-17 23:51:56 -0700

    Exercise 1

---
+++ js/exercise1.js
@@ -0,0 +1,11 @@
+// Write an algorithm and draw a flowchart to convert the length in feet to centimeters.
+
+// Pseudocode:
+
+// - Input length in feet (LFT)
+// - Calculate the length in cm (LCM) by multiplying LFT by 30
+// - Print length in cm (LCM)
+
+let LFT = 100
+let LCM = LFT * 30
+console.log(LCM) 
