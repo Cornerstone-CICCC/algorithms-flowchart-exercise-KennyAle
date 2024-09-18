7ac0cc5c50dc0c20a71f6c558fd3341cb1cd5f01
KennyAle <kennyviver@gmail.com>
2024-09-17 23:52:12 -0700

    Exercise 2 algorithm

---
+++ js/exercise2.js
@@ -0,0 +1,12 @@
+// Write an algorithm and draw a flowchart that will read the two sides of a rectangle and calculate its area.
+
+// Pseudocode:
+
+// - Input the width (W) and the length (L) of a rectangle
+// - Calculate the area (A) by multiplying W by L
+// - Print A
+
+let W = 10
+let L = 5
+let A = W * L
+console.log(A)
