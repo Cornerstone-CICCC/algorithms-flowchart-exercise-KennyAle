fe6e2fca259fe50e2efdbdc2205280e1c2b703d0
KennyAle <kennyviver@gmail.com>
2024-09-17 23:52:21 -0700

    Exercise 3 algorithm

---
+++ js/exercise3.js
@@ -0,0 +1,15 @@
+// Write an algorithm and draw a flowchart that will read three numbers and prints the value of the largest number.
+
+// > Given that the three numbers are not equal to each other)
+
+let a = 12
+let b = 2
+let c = 3
+
+if (a > b && a > c) {
+    console.log(`A is the GOAT with a value of ${a}`)
+} else if (b > a && b > c) {
+    console.log(`B is the GOAT with a value of ${b}`)
+} else {
+    console.log(`C is the GOAT with a value of ${c}`)
+}
